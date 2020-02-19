24c8a709c5f378a6036ef16cc692a4ec0a4c87da
Mello <mello+github@ankot.org>
2020-02-20 00:00:06 +0100

    awsibox/replicator.py

---
+++ awsibox/replicator.py
@@ -0,0 +1,34 @@
+import troposphere.cloudformation as clf
+
+from .common import *
+from .shared import (Parameter, do_no_override, get_endvalue, get_expvalue,
+                     get_subvalue, auto_get_props, get_condition, add_obj)
+
+
+class CLF_CustomResourceReplicator(object):
+    def __init__(self, key):
+        # Parameters
+        P_ReplicateRegions = Parameter('ReplicateRegions')
+        P_ReplicateRegions.Description = (
+            'Regions where to replicate - empty for default based on env/role')
+        P_ReplicateRegions.Type = 'CommaDelimitedList'
+
+        add_obj(P_ReplicateRegions)
+
+        # Resources
+        R_Replicator = clf.CustomResource(
+            'CloudFormationCustomResourceStackReplicator')
+
+        if 'LambdaStackReplicator' in cfg.Resources:
+            R_Replicator.ServiceToken = GetAtt('LambdaStackReplicator', 'Arn')
+        else:
+            R_Replicator.ServiceToken = get_expvalue('LambdaStackReplicator')
+
+        for p, v in cfg.Parameters.items():
+            if not p.startswith('Env'):
+                value = get_endvalue(p)
+                setattr(R_Replicator, p, value)
+
+        R_Replicator.StackName = Ref('AWS::StackName')
+
+        add_obj(R_Replicator)
