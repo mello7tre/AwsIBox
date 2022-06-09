4b684011ddb1a3a0d078bfffd4f807013864ac39
Mello <mello+github@ankot.org>
2022-06-10 00:00:41 +0200

    Joker, can use IBOX_RESNAME key for resname value
    If exist key IBOX_RESNAME
    mapname = resname
    resname = IBOX_RESNAME key value
    
    useful to move R53_RecordSetCCH to joker

--- awsibox/joker.py
+++ awsibox/joker.py
@@ -7,14 +7,21 @@ def Joker(key, module, cls):
         if not v.get("IBOX_ENABLED", True):
             continue
 
+        mapname = None
         resname = f"{key}{n}"
+
+        # change resource name using key IBOX_RESNAME
+        ibox_resname = v.get("IBOX_RESNAME")
+        if ibox_resname:
+            mapname = resname
+            resname = ibox_resname
 
         mod = __import__(f"troposphere.{module}")
         my_module = getattr(mod, module)
         my_class = getattr(my_module, cls)
 
         res = my_class(resname)
-        auto_get_props(res, indexname=n)
+        auto_get_props(res, mapname=mapname, indexname=n)
 
         if v.get("Create"):
             add_obj(
