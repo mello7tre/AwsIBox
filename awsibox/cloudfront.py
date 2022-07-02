dd6ded90de2af06c5ba048d77977502c592a14e2
Mello <mello+github@ankot.org>
2022-07-03 00:01:43 +0200

    CloudFront reactor
    I use process cache behaviours and origins with custom function, then i
    use IBOX_SUB_OBJ to get them, and finally i create the distribution with
    a single auto_get_props
    
    maybe need some other fixes...

--- awsibox/cloudfront.py
+++ awsibox/cloudfront.py
@@ -51,23 +51,6 @@ class CFCacheBehavior(clf.CacheBehavior, CFDefaultCacheBehavior):
         self.PathPattern = get_endvalue(f"{name}PathPattern")
 
 
-def CFCustomErrors():
-    CustomErrorResponses = []
-    mapname = "CloudFrontCustomErrorResponses"
-
-    try:
-        ErrorResponses = getattr(cfg, mapname)
-    except Exception:
-        pass
-    else:
-        for n, v in ErrorResponses.items():
-            resname = f"{mapname}{n}"
-            CustomErrorResponse = clf.CustomErrorResponse(resname)
-            CustomErrorResponses.append(auto_get_props(CustomErrorResponse))
-
-    return CustomErrorResponses
-
-
 def CFOrigins():
     Origins = []
     for n, v in cfg.CloudFrontOrigins.items():
@@ -175,3 +158,51 @@ def CF_CloudFront(key):
     R_CloudFrontDistribution = CloudFrontDistribution
 
     add_obj([R_CloudFrontDistribution])
+
+
+def cache_behavior_process():
+    for n, v in cfg.CloudFrontCacheBehaviors.items():
+        resname = f"CloudFrontCacheBehaviors{n}"
+        # Use CachePolicyId/OriginRequestPolicyId or legacy mode
+        if "CachePolicyId" in v:
+            for k in ["DefaultTTL", "MaxTTL", "MinTTL", "ForwardedValues"]:
+                try:
+                    del getattr(cfg, resname)[k]
+                except Exception:
+                    pass
+
+def origin_process():
+    for n, v in cfg.CloudFrontOrigins.items():
+        resname = f"CloudFrontOrigins{n}"
+
+        if "OriginAccessIdentity" in v.get("S3OriginConfig", []):
+            del getattr(cfg, resname)["CustomOriginConfig"]
+        else:
+            del getattr(cfg, resname)["S3OriginConfig"]
+
+
+def CF_CloudFront(key):
+    # Resources
+    R_CloudFrontDistribution = clf.Distribution("CloudFrontDistribution")
+
+    # process cache_behavior and origin
+    cache_behavior_process()
+    origin_process()
+
+    # Automatically compute Behaviour Order based on PathPattern
+    cfg.dbg_clf_compute_order = {}
+    sortedcachebehaviors = sorted(
+        cfg.CloudFrontCacheBehaviors.items(), key=lambda x_y: clf_compute_order(x_y[1]["PathPattern"])
+    )
+    cfg.CloudFrontCacheBehaviors = {x[0]: x[1] for x in sortedcachebehaviors}
+
+    if cfg.debug:
+        print("##########CLF_COMPUTE_ORDER#########START#######")
+        for n, v in iter(
+            sorted(cfg.dbg_clf_compute_order.items(), key=lambda x_y: x_y[1])
+        ):
+            print(f"{n} {v}\n")
+        print("##########CLF_COMPUTE_ORDER#########END#######")
+
+    auto_get_props(R_CloudFrontDistribution, mapname="CloudFrontDistributionBase")
+    add_obj(R_CloudFrontDistribution)
