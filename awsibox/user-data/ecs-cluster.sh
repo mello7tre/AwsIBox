b0292ff7cc4b39efe3fc0f0688a4ecd357be1b0b
Mello <mello+github@ankot.org>
2022-11-16 00:00:15 +0100

    ecs-cluster user-data - use IMDSv2

--- awsibox/user-data/ecs-cluster.sh
+++ awsibox/user-data/ecs-cluster.sh
@@ -11,7 +11,8 @@ signal(){
      status=FAILURE
      ;;
   esac
-  instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+  IMDSv2_token=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+  instance_id=$(curl -H "X-aws-ec2-metadata-token: $IMDSv2_token" -s http://169.254.169.254/latest/meta-data/instance-id)
   # avoid exiting with error if stack is in UPDATE_COMPLETE state and cannot be signaled
   aws --region $REGION cloudformation signal-resource --stack-name $STACKNAME --logical-resource-id AutoScalingGroup --unique-id $instance_id --status $status || :
 
