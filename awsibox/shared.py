0da6fa06ea0766a7189b425e704fde9bdbf1dd3f
Mello <mello+github@ankot.org>
2023-06-16 00:00:08 +0200

    python_minifier.minify, need to use preserve_shebang=False with newer version

--- awsibox/shared.py
+++ awsibox/shared.py
@@ -531,7 +531,7 @@ def import_lambda(name):
 
             try:
                 # try to minify using python_minifier
-                code = python_minifier.minify(fdata)
+                code = python_minifier.minify(fdata, preserve_shebang=False)
                 if len(code) > 4096:
                     logging.warning(
                         f"{lambda_file_trunk} > 4096, trying to minify it using a more aggressive option [rename_globals=True]"
