4cdbdc160a8a137011668d0fb3417bbf0aa61382
Mello <mello+github@ankot.org>
2025-09-29 00:00:03 +0200

    local_install_git_version.sh

--- local_install_git_version.sh
+++ local_install_git_version.sh
@@ -2,4 +2,5 @@
 COMMIT=$(git rev-parse --short HEAD)
 sed -i "s#\(__version__ =[^\"]\+\"[^\" ]\+\).*#\1+$COMMIT\"#" awsibox/__init__.py
 pip3 install .
+rm -fr awsibox.egg-info build
 git checkout awsibox/__init__.py
