fc222404eb4593d3efde8172ba56df6ef7531344
Mello <mello+github@ankot.org>
2026-07-17 00:00:00 +0200

    mod/cloudfront.py clean up
    use IBOX_ENABLED_IF in ForwardedValues and DefaultTTL.IBOX_CODE_KEY to
    replace process_cache_policy (remove DefaultTTL and ForwardedValues if
    using CachePolicyId)

--- awsibox/mod/cloudfront.py
+++ awsibox/mod/cloudfront.py
@@ -4,16 +4,6 @@ from .. import cfg
 from ..shared import auto_get_props, add_obj, clf_compute_order
 
 
-def process_cache_policy(v):
-    # Use CachePolicyId/OriginRequestPolicyId or legacy mode
-    if "CachePolicyId" in v:
-        for k in ["DefaultTTL", "MaxTTL", "MinTTL", "ForwardedValues"]:
-            try:
-                del v[k]
-            except Exception:
-                pass
-
-
 def CF_CloudFront(key):
     for n, v in getattr(cfg, key).items():
         if not v.get("IBOX_ENABLED", True):
@@ -22,11 +12,6 @@ def CF_CloudFront(key):
         # Resources
         R_CloudFrontDistribution = cloudfront.Distribution(resname)
         distribution_config = v["DistributionConfig"]
-
-        # process cache behaviors
-        process_cache_policy(distribution_config["DefaultCacheBehavior"])
-        for m, w in distribution_config["CacheBehaviors"].items():
-            process_cache_policy(w)
 
         # Automatically compute Behaviour Order based on PathPattern
         cfg.dbg_clf_compute_order = {}
